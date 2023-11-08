4b8afa1243c651d1e62e83b8d182ceae627d5813
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 15:04:33 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+// const button=document.querySelector("input")[0];
+function solve(){
+	const select=document.querySelector("#colorSelect");
+ const selectedvalue=select.value;
+	let option=select.children;
+	for(let i=0;i<option.length;i++){
+		if(option[i].value==selectedvalue){
+			option[i].remove();
+			return;
+		}
+	}
+}
